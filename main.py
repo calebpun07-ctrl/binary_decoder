022dcafafb2da66617b99b814a1cc0baaaad0c56
calebpun07-ctrl <calebpun07@gmail.com>
2025-09-05 11:21:37 -0400

    two files, one with binary and anoteh riwth its text translation

---
+++ main.py
@@ -0,0 +1,14 @@
+with open('binary.txt', 'r') as f:
+    lines = f.readlines() # or content = f.read()
+
+adder = []
+
+# Example: Replace a word on a specific line
+for i, line in enumerate(lines):
+    
+    adder.append(line[9]+"\n")
+
+# Example: Add a new line
+
+with open('translation.txt', 'w') as f:
+    f.writelines(adder) # or f.write(content)
